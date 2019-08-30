8d91f92c039b90cb55eddeefc0f288ce77d417f6
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2019-08-30 11:56:07 +0200

    🔖 v0.1.0 → v0.2.0

--- pyjapi.py
+++ pyjapi.py
@@ -7,7 +7,7 @@ import sys
 
 import click
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 
 class JAPIClient():
@@ -110,7 +110,7 @@ class JAPIClient():
 @click.option(
     "--host",
     envvar="JAPI_HOST",
-    default="127.0.1.0",
+    default="127.0.2.0",
     help="JAPI server hostname or ip",
     type=click.STRING,
 )
