60d10737698b483bdeebede124b9f3418c048564
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2020-04-21 13:52:49 +0200

    fix tests
    - fix issue with relative path

--- tests/test_pyjapi.py
+++ tests/test_pyjapi.py
@@ -1,15 +1,16 @@
+import pathlib
 from subprocess import run, CompletedProcess
 
 
 def test_execution_via_module():
-    r: CompletedProcess = run(["python", "-m", "pyjapi"], capture_output=True)
+    r: CompletedProcess = run(["python3", "-m", "pyjapi"], capture_output=True)
     assert r.returncode == 0
     assert r.stdout.decode().startswith("Usage: japi [OPTIONS] COMMAND [ARGS]...")
     assert r.stderr.decode() == ''
 
 
 def test_direct_execution():
-    r: CompletedProcess = run(["./src/pyjapi/cli.py"], capture_output=True)
+    r: CompletedProcess = run([pathlib.Path(__file__).parent.parent/"src"/"pyjapi"/"cli.py"], capture_output=True)
     assert r.returncode == 0
     assert r.stdout.decode().startswith("Usage: japi [OPTIONS] COMMAND [ARGS]...")
     assert r.stderr.decode() == ''
