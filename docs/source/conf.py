6c457c503a88348a84264c93156bf38b3007acca
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2022-07-28 16:34:16 +0200

    doc: recreate docs with Sphinx 5.1.1
    * remove path manipulation in `conf.py`

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,38 +1,18 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-import os
-import pathlib
-import sys
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-import sphinx_nameko_theme
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-sys.path.insert(0, pathlib.Path(__file__).parent.parent.parent/'src')
-# include source directory to load custom extensions
-sys.path.insert(0, os.path.abspath(os.path.dirname('__file__')))
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'pyjapi'
-copyright = '2020, Jannis Mainczyk'
+copyright = '2022, Jannis Mainczyk'
 author = 'Jannis Mainczyk'
-
-# The full version, including alpha/beta/rc tags
 release = '0.5.2'
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -82,6 +62,9 @@ source_suffix = {
 default_role = 'py:obj'
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+import sphinx_nameko_theme
 
 html_theme_path = [sphinx_nameko_theme.get_html_theme_path()]
 html_theme = 'nameko'
@@ -95,6 +78,7 @@ html_logo = '_static/logo.png'
 
 # -- Options for Confluence Builder ------------------------------------------
 # https://sphinxcontrib-confluencebuilder.readthedocs.io/
+import os # read confluence password from env
 
 confluence_publish = True
 confluence_server_url = 'https://intern.iis.fhg.de/'
